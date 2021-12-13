b3f3a82364b76f4ca6abef5d91e6f448ffae2fec
Henry Griffiths <henrygriffiths@improbable.io>
2021-12-13 13:26:44 +0000

    Clean up driver, add detailed "sample" panel

--- main.cc
+++ main.cc
@@ -12,38 +12,51 @@
 // ---------------- MAIN ----------
 int main() {
 
+    int NUM_TRADERS_EACH_TYPE = 10;
+    int NUM_TICKS = 50;
+
+    double STARTING_MONEY = 20.0;
+    int SAMPLE_ID = 2;
+
     std::vector<std::pair<double, double>> avg_food_price, avg_food_trades, avg_food_asks, avg_food_bids;
     std::vector<std::pair<double, double>> avg_wood_price, avg_wood_trades, avg_wood_asks, avg_wood_bids;
 
     std::vector<std::pair<double, double>> num_alive_farmers;
     std::vector<std::pair<double, double>> num_alive_woodcutters;
 
+    std::vector<std::pair<double, double>> sample_money;
+    std::vector<std::pair<double, double>> sample_food;
+    std::vector<std::pair<double, double>> sample_wood;
+    std::vector<std::pair<double, double>> sample_tools;
+
     // Setup scenario
     auto food = Commodity("food");
     auto wood = Commodity("wood");
     auto tools = Commodity("tools");
 
-    auto auction_house = std::make_shared<AuctionHouse>(0, Log::DEBUG);
+    auto auction_house = std::make_shared<AuctionHouse>(0, Log::INFO);
     auction_house->RegisterCommodity(food);
     auction_house->RegisterCommodity(wood);
     auction_house->RegisterCommodity(tools);
 
     std::vector<std::shared_ptr<AITrader>> all_traders;
-    std::vector<InventoryItem> DefaultFarmerInv{{food, 0, 0}, {wood, 0, 3}, {tools, 1, 1}};
+    std::vector<InventoryItem> DefaultFarmerInv{{food, 0, 0}, {wood, 1, 3}, {tools, 1, 1}};
     std::vector<InventoryItem> DefaultWoodcutterInv{{food, 1, 3}, {wood, 0, 0}, {tools, 1, 1}};
 
     int max_id = 1;
     std::shared_ptr<AITrader> new_trader;
-    for (int i = 0; i < 4; i++) {
-        new_trader = CreateAndRegister(max_id, auction_house, std::make_shared<RoleFarmer>(), "farmer", 20.0, 20, DefaultFarmerInv, Log::DEBUG);
+    for (int i = 0; i < NUM_TRADERS_EACH_TYPE; i++) {
+        new_trader = CreateAndRegister(max_id, auction_house, std::make_shared<RoleFarmer>(), "farmer", STARTING_MONEY, 20, DefaultFarmerInv, Log::WARN);
         all_traders.push_back(new_trader);
         max_id++;
-        new_trader = CreateAndRegister(max_id, auction_house, std::make_shared<RoleWoodcutter>(), "woodcutter", 20.0, 20, DefaultWoodcutterInv, Log::DEBUG);
+        new_trader = CreateAndRegister(max_id, auction_house, std::make_shared<RoleWoodcutter>(), "woodcutter", STARTING_MONEY, 20, DefaultWoodcutterInv, Log::WARN);
         all_traders.push_back(new_trader);
         max_id++;
     }
 
-    for (int curr_tick = 0; curr_tick < 10; curr_tick++) {
+    all_traders[SAMPLE_ID]->logger.verbosity = Log::DEBUG;
+
+    for (int curr_tick = 0; curr_tick < NUM_TICKS; curr_tick++) {
         int num_farmers = 0;
         int num_woodcutters = 0;
 
@@ -77,6 +90,11 @@ int main() {
         avg_wood_trades.emplace_back(curr_tick, auction_house->AverageHistoricalTrades("wood", 1));
         avg_wood_asks.emplace_back(curr_tick, auction_house->AverageHistoricalAsks("wood", 1));
         avg_wood_bids.emplace_back(curr_tick, auction_house->AverageHistoricalBids("wood", 1));
+
+        sample_money.emplace_back(curr_tick, all_traders[SAMPLE_ID]->money);
+        sample_food.emplace_back(curr_tick, all_traders[SAMPLE_ID]->Query("food"));
+        sample_wood.emplace_back(curr_tick, all_traders[SAMPLE_ID]->Query("wood"));
+        sample_tools.emplace_back(curr_tick, all_traders[SAMPLE_ID]->Query("tools"));
     }
 
 
@@ -103,5 +121,13 @@ int main() {
     plots = gp.plotGroup();
     plots.add_plot1d(num_alive_farmers, "with lines title 'Farmers'");
     plots.add_plot1d(num_alive_woodcutters, "with lines title 'Woodcutters'");
+    gp << plots;
+
+    gp << "set title 'Sample Trader Detail'\n";
+    plots = gp.plotGroup();
+    plots.add_plot1d(sample_money, "with lines title 'Money'");
+    plots.add_plot1d(sample_food, "with lines title 'Food'");
+    plots.add_plot1d(sample_wood, "with lines title 'Wood'");
+    plots.add_plot1d(sample_tools, "with lines title 'Tools'");
     gp << plots;
 }
