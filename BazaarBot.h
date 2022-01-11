197695cb91344ae674b8115f6921a871aa9e4b19
Henry Griffiths <henrygriffiths@improbable.io>
2022-01-11 10:13:39 +0000

    Moved some utility functions out of main and into the library

--- BazaarBot.h
+++ BazaarBot.h
@@ -3,36 +3,23 @@
 
 
 #include "common/concurrency.h"
-
+#include "common/agent.h"
+#include "common/messages.h"
 #include "common/commodity.h"
+
 #include "traders/inventory.h"
 
-#include "common/agent.h"
-
 #include "auction/auction_house.h"
-#include "common/messages.h"
+
 #include "metrics/logger.h"
+#include "metrics/metrics.h"
+#include "metrics/display.h"
+
 #include "traders/AI_trader.h"
 #include "traders/fake_trader.h"
 #include "traders/human_trader.h"
 #include "traders/roles.h"
 
-#include "metrics/display.h"
-
-std::shared_ptr<AITrader> CreateAndRegisterBasic(int id,
-                                                    const std::vector<std::pair<Commodity, int>>& inv,
-                                                    const std::shared_ptr<AuctionHouse>& auction_house) {
-
-    std::vector<InventoryItem> inv_vector;
-    for (const auto &item : inv) {
-        inv_vector.emplace_back(item.first.name, item.second);
-    }
-    auto trader = std::make_shared<AITrader>(id, auction_house, std::nullopt, "test_class", 100.0, 50, inv_vector, Log::WARN);
-
-    trader->SendMessage(*Message(id).AddRegisterRequest(std::move(RegisterRequest(trader->id, trader))), auction_house->id);
-    trader->TickOnce();
-    return trader;
-}
 std::shared_ptr<AITrader> CreateAndRegister(int id,
                                                const std::shared_ptr<AuctionHouse>& auction_house,
                                                std::shared_ptr<Role> AI_logic,
@@ -50,6 +37,51 @@ std::shared_ptr<AITrader> CreateAndRegister(int id,
     return trader;
 }
 
+int RandomChoice(int num_weights, std::vector<double>& weights, std::mt19937& gen) {
+    double sum_of_weight = 0;
+    for(int i=0; i<num_weights; i++) {
+        sum_of_weight += weights[i];
+    }
+    std::uniform_real_distribution<> random(0, sum_of_weight);
+    double rnd = random(gen);
+    for(int i=0; i<num_weights; i++) {
+        if(rnd < weights[i])
+            return i;
+        rnd -= weights[i];
+    }
+    return -1;
+}
+
+std::string GetProducer(std::string& commodity) {
+    if (commodity == "food") {
+        return "farmer";
+    } else if (commodity == "fertilizer") {
+        return "composter";
+    } else if (commodity == "wood") {
+        return "woodcutter";
+    } else if (commodity == "ore") {
+        return "miner";
+    } else if (commodity == "metal") {
+        return "refiner";
+    } else if (commodity == "tools") {
+        return "blacksmith";
+    } else {
+        return "null";
+    }
+}
+std::string ChooseNewClassWeighted(std::vector<std::string>& tracked_goods, std::shared_ptr<AuctionHouse>& auction_house, std::mt19937& gen) {
+    std::vector<double> weights;
+    double gamma = -0.02;
+    //auction house ticks at 10ms
+    int lookback_time_ms = 1000;
+    for (auto& commodity : tracked_goods) {
+        double supply = auction_house->t_AverageHistoricalSupply(commodity, lookback_time_ms);
+//        double supply = auction_house->AverageHistoricalAsks(commodity, 100) - auction_house->AverageHistoricalBids(commodity, 100);
+        weights.push_back(std::exp(gamma*supply));
+    }
+    int choice = RandomChoice((int) weights.size(),  weights, gen);
+    return GetProducer(tracked_goods[choice]);
+}
 
 
 #endif //BAZAAR_BOT_H
--- main.cc
+++ main.cc
@@ -3,7 +3,6 @@
 //
 #include <iostream>
 #include "BazaarBot.h"
-#include "metrics/metrics.h"
 #include <vector>
 #include <chrono>
 #include <thread>
@@ -40,51 +39,7 @@ std::string ChooseNewClassRandom(std::vector<std::string>& tracked_roles, std::m
     return tracked_roles[new_job];
 }
 
-int RandomChoice(int num_weights, std::vector<double>& weights, std::mt19937& gen) {
-    double sum_of_weight = 0;
-    for(int i=0; i<num_weights; i++) {
-        sum_of_weight += weights[i];
-    }
-    std::uniform_real_distribution<> random(0, sum_of_weight);
-    double rnd = random(gen);
-    for(int i=0; i<num_weights; i++) {
-        if(rnd < weights[i])
-            return i;
-        rnd -= weights[i];
-    }
-    return -1;
-}
 
-std::string GetProducer(std::string& commodity) {
-    if (commodity == "food") {
-        return "farmer";
-    } else if (commodity == "fertilizer") {
-        return "composter";
-    } else if (commodity == "wood") {
-        return "woodcutter";
-    } else if (commodity == "ore") {
-        return "miner";
-    } else if (commodity == "metal") {
-        return "refiner";
-    } else if (commodity == "tools") {
-        return "blacksmith";
-    } else {
-        return "null";
-    }
-}
-std::string ChooseNewClassWeighted(std::vector<std::string>& tracked_goods, std::shared_ptr<AuctionHouse>& auction_house, std::mt19937& gen) {
-    std::vector<double> weights;
-    double gamma = -0.02;
-    //auction house ticks at 10ms
-    int lookback_time_ms = 1000;
-    for (auto& commodity : tracked_goods) {
-        double supply = auction_house->t_AverageHistoricalSupply(commodity, lookback_time_ms);
-//        double supply = auction_house->AverageHistoricalAsks(commodity, 100) - auction_house->AverageHistoricalBids(commodity, 100);
-        weights.push_back(std::exp(gamma*supply));
-    }
-    int choice = RandomChoice((int) weights.size(),  weights, gen);
-    return GetProducer(tracked_goods[choice]);
-}
 
 void Run(double duration_s, double animation_fps, double trader_tps) {
     int NUM_TRADERS_EACH_TYPE = 10;
